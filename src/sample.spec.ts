5394beb4cbca580a5804dc236a5ea42e818192c4
Alex <alex.bogatov1994@gmail.com>
2024-10-03 18:38:59 -0400

    added test test

---
+++ src/sample.spec.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+
+describe('тест', () => {
+  it('тест прошел', () => {
+    const result = 1 + 1;
+    expect(result).to.equal(2);
+  });
+});
---
+++ src/tools/Block.spec.ts
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+
+describe('Block', () => {
+  it('тест прошел', () => {
+    const result = 1 + 1;
+    expect(result).to.equal(2);
+  });
+});
+
